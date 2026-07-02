d89c477749ec14275ccc20afffc989c937f27bc8
Mohannad Ali <mohannad.eali@gmail.com>
2026-07-01 19:48:20 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+const app = document.querySelector("#app");
+
+const heading = document.createElement("h1");
+heading.textContent = "Webpack template is working";
+
+app.appendChild(heading);
---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: {
+    main: "./src/index.js",
+  },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "assets/[name][ext]",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/template.html",
+      filename: "index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
